6c00d5af9f270566af442882e7a2216f13e079bc
tshea68 <timothyshea1@gmail.com>
2025-07-19 14:16:37 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import SingleProduct from "./singleproduct"; // adjust path if needed
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/part/:mpn" element={<SingleProduct />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+
+
 
 
