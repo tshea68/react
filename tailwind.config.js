3fcdcea4d085ece3b455ce66c26d842284e7d9e1
tshea68 <timothyshea1@gmail.com>
2025-08-22 10:54:44 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+// tailwind.config.js  (CommonJS)
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: "480px",  // large phones
+        pad:   "768px",  // tablets
+        lap:   "1024px", // laptops
+        desk:  "1440px", // large desktops
+      },
+    },
   },
+  // (variants is legacy in Tailwind v3+, but leaving it won't break)
   variants: {
     extend: {
       display: ["group-hover"],
@@ -10,3 +19,4 @@ module.exports = {
   },
   plugins: [],
 };
+
