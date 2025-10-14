aab259744a42df72742405ad63caa96a88d4e6c7
tshea68 <timothyshea1@gmail.com>
2025-10-14 11:43:59 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,27 @@ import { CartProvider } from "./context/CartContext";
 
 import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
-import SingleProduct from "./SingleProduct";      // keep your current path/casing
+import SingleProduct from "./SingleProduct";
 import ModelPage from "./ModelPage";
 import CartPage from "./pages/CartPage";
-import CheckoutPage from "./pages/CheckoutPage";  // ✅ added
-import SuccessPage from "./pages/SuccessPage";    // ✅ added
+import CheckoutPage from "./pages/CheckoutPage";
+import SuccessPage from "./pages/SuccessPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout><HomePage /></Layout>} />
-          <Route path="/parts/:mpn" element={<Layout><SingleProduct /></Layout>} />
-          <Route path="/refurb/:mpn" element={<Layout><SingleProduct /></Layout>} />
-          <Route path="/model" element={<Layout><ModelPage /></Layout>} />
-          <Route path="/cart" element={<Layout><CartPage /></Layout>} />
-          <Route path="/checkout" element={<Layout><CheckoutPage /></Layout>} /> {/* ✅ */}
-          <Route path="/success" element={<Layout><SuccessPage /></Layout>} />   {/* ✅ */}
+          {/* Wrap all app routes with the Layout. Layout should render <Outlet /> */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/parts/:mpn" element={<SingleProduct />} />
+            <Route path="/refurb/:mpn" element={<SingleProduct />} />
+            <Route path="/model" element={<ModelPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/success" element={<SuccessPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </CartProvider>
