c2031a97277a04c7c5916202913cb8ac0bc74417
tshea68 <timothyshea1@gmail.com>
2025-11-10 21:15:35 -0500

    Update ModelPage.jsx

--- src/ModelPage.jsx
+++ src/ModelPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useMemo, useRef } from "react";
 import { useSearchParams, Link } from "react-router-dom";
 import PartImage from "./components/PartImage";
 
-const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = "https://api.appliancepartgeeks.com";
 
 /* ---------------- helpers ---------------- */
 const normalize = (s) =>
