e0cfbd71dbb83fd9c66be55d8877a40ac02cbe26
tshea68 <timothyshea1@gmail.com>
2025-11-12 14:25:49 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,13 @@ import { CartProvider } from "./context/CartContext";
 
 import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
+import SingleProductRetail from "./components/SingleProductRetail.jsx";
+import SingleProductOffer from "./components/SingleProductOffer.jsx";
 import ModelPage from "./ModelPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import SuccessPage from "./pages/SuccessPage";
 import PartsExplorerPage from "./pages/PartsExplorerPage.jsx";
-
-// 🔁 Unified product page
-import SingleProduct from "./SingleProduct.jsx"; // <- the single page to use for both routes
 
 function NotFound() {
   return <div className="p-6 text-sm text-gray-600">Page not found.</div>;
@@ -25,86 +24,17 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <CartProvider>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />
-              </Layout>
-            }
-          />
-
-          {/* Both OEM and Refurb routes go to the same SingleProduct page */}
-          <Route
-            path="/parts/:mpn"
-            element={
-              <Layout>
-                <SingleProduct />
-              </Layout>
-            }
-          />
-          <Route
-            path="/refurb/:mpn"
-            element={
-              <Layout>
-                <SingleProduct />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/model"
-            element={
-              <Layout>
-                <ModelPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/cart"
-            element={
-              <Layout>
-                <CartPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/checkout"
-            element={
-              <Layout>
-                <CheckoutPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/grid"
-            element={
-              <Layout>
-                <PartsExplorerPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/success"
-            element={
-              <Layout>
-                <SuccessPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="*"
-            element={
-              <Layout>
-                <NotFound />
-              </Layout>
-            }
-          />
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/parts/:mpn" element={<SingleProductRetail />} />
+            <Route path="/refurb/:mpn" element={<SingleProductOffer />} />
+            <Route path="/model" element={<ModelPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/grid" element={<PartsExplorerPage />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </CartProvider>
