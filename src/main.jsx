9855859f2054f3ca1907cf8e99e2cdb555c41edf
tshea68 <timothyshea1@gmail.com>
2025-11-12 11:13:05 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,84 +15,26 @@ import CheckoutPage from "./pages/CheckoutPage";
 import SuccessPage from "./pages/SuccessPage";
 import PartsExplorerPage from "./pages/PartsExplorerPage.jsx";
 
+function NotFound() {
+  return <div className="p-6 text-sm text-gray-600">Page not found.</div>;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />
-              </Layout>
-            }
-          />
-
-          {/* OEM / regular part detail page */}
-          <Route
-            path="/parts/:mpn"
-            element={
-              <Layout>
-                <SingleProductRetail />
-              </Layout>
-            }
-          />
-
-          {/* Refurb / marketplace offer detail page */}
-          <Route
-            path="/refurb/:mpn"
-            element={
-              <Layout>
-                <SingleProductOffer />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/model"
-            element={
-              <Layout>
-                <ModelPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/cart"
-            element={
-              <Layout>
-                <CartPage />
-              </Layout>
-            }
-          />
-
-          <Route
-            path="/checkout"
-            element={
-              <Layout>
-                <CheckoutPage />
-              </Layout>
-            }
-          />
-         
-          <Route
-            path="/grid"
-            element={
-              <Layout>
-                <PartsExplorerPage />
-              </Layout>
-            }
-          />
-         
-          <Route
-            path="/success"
-            element={
-              <Layout>
-                <SuccessPage />
-              </Layout>
-            }
-          />
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/parts/:mpn" element={<SingleProductRetail />} />
+            <Route path="/refurb/:mpn" element={<SingleProductOffer />} />
+            <Route path="/model" element={<ModelPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/grid" element={<PartsExplorerPage />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </CartProvider>
