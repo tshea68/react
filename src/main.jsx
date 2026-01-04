530f4775e5227a09c2156e2dfb3e1e86a2bb1aad
tshea68 <timothyshea1@gmail.com>
2026-01-03 19:35:35 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+import { HelmetProvider } from "react-helmet-async"; // ✅ SEO head manager
 
 import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
@@ -22,31 +23,33 @@ function NotFound() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
+    <HelmetProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
 
-            {/* ✅ Facet links: support /grid as an alias to the PartsExplorer */}
-            <Route path="/grid" element={<HomePage />} />
+              {/* ✅ Facet links: support /grid as an alias to the PartsExplorer */}
+              <Route path="/grid" element={<HomePage />} />
 
-            {/* ✅ Public order status page (opaque token) */}
-            <Route path="/order/:token" element={<OrderStatusPage />} />
+              {/* ✅ Public order status page (opaque token) */}
+              <Route path="/order/:token" element={<OrderStatusPage />} />
 
-            {/* Unified product page: works for both retail + refurb */}
-            <Route path="/parts/:mpn" element={<SingleProduct />} />
-            <Route path="/refurb/:mpn" element={<SingleProduct />} />
+              {/* Unified product page: works for both retail + refurb */}
+              <Route path="/parts/:mpn" element={<SingleProduct />} />
+              <Route path="/refurb/:mpn" element={<SingleProduct />} />
 
-            <Route path="/model" element={<ModelPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/success" element={<SuccessPage />} />
+              <Route path="/model" element={<ModelPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/success" element={<SuccessPage />} />
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
